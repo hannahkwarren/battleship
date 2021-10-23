6c18bcdd99fd52ab4c5b8002b42175f7a890fa21
Hannah Warren <hannahkwarren@gmail.com>
2021-10-23 17:29:39 -0400

    Feat: finish game setup with the user-input placements

--- spec/play_spec.rb
+++ spec/play_spec.rb
@@ -25,9 +25,13 @@ RSpec.describe Play do
 
   it 'generates random_computer_placements' do
     expect(@play.random_computer_placements).to eq(true)
+    #need to ask at our project checkin about a way to test
+    # that the placed cells have a ship object
   end
 
-  xit 'inputs player_placements' do
+  it 'inputs player_placements' do
+    #need to ask at checkin about a way to verify
+    # that placed cells have a ship object
     expect(@play.player_placements).to eq()
   end
 
