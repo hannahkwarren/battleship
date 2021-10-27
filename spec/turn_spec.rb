98a1358b28e852080c94a0e17d5fa74a319e9e04
Sierra Tucker <sierra.tucker121@gmail.com>
2021-10-26 19:05:50 -0600

    Test: Add and refactor final Turn class tests

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -9,6 +9,16 @@ RSpec.describe Turn do
   before(:each) do
     @play = Play.new
     @turn = Turn.new(@play)
+    @play.computer_board = Board.new
+    @play.player_board = Board.new
+    @play.computer_ships = [Ship.new("Cruiser", 3), Ship.new("Submarine", 2)]
+    @play.player_ships = [Ship.new("Cruiser", 3), Ship.new("Submarine", 2)]
+    @turn.computer_turn
+    @possible_shots = @play.player_board.cells.values.select {|cell| cell.fired_upon? == false}
+    @computer_shot = @possible_shots.sample
+    @computer_shot.fire_upon
+    @player_shot = "A4"
+    @play.computer_board.cells[@player_shot].fire_upon
   end
 
   it 'exists' do
@@ -17,31 +27,22 @@ RSpec.describe Turn do
 
   describe 'it has attributes' do
     it '#computer_shot' do
-      @play.computer_board = Board.new
-      @play.player_board = Board.new
-      @play.computer_ships = [Ship.new("Cruiser", 3), Ship.new("Submarine", 2)]
-      @play.player_ships = [Ship.new("Cruiser", 3), Ship.new("Submarine", 2)]
-      @turn.computer_turn
-      possible_shots = @play.player_board.cells.values.select {|cell| cell.fired_upon? == false}
-      computer_shot = possible_shots.sample
-      computer_shot.fire_upon
       expect(@turn.computer_shot.fired_upon?).to eq(true)
       expect(@turn.computer_shot.class).to be(Cell)
     end
 
     it '#player_shot' do
-      @play.computer_board = Board.new
-      @play.player_board = Board.new
-      @play.computer_ships = [Ship.new("Cruiser", 3), Ship.new("Submarine", 2)]
-      @play.player_ships = [Ship.new("Cruiser", 3), Ship.new("Submarine", 2)]
-      player_shot = "A4" #This is test-added user input
-      @play.computer_board.cells[player_shot].fire_upon
-      expect(@play.computer_board.cells[player_shot].fired_upon?).to eq(true)
-      expect(@play.computer_board.cells[player_shot].class).to be(Cell)
+      expect(@play.computer_board.cells[@player_shot].fired_upon?).to eq(true)
+      expect(@play.computer_board.cells[@player_shot].class).to be(Cell)
     end
 
-    xit '#has_results' do
+    it '#has_results' do
+      player_turn_result = @play.computer_board.cells[@player_shot].render()
+      comp_turn_result = @computer_shot.render
 
+      expect(@turn.results.class).to be(Hash)
+      expect(player_turn_result.class).to be(String)
+      expect(comp_turn_result.class).to be(String)
     end
   end
 end
